fc17041b37db6cf0e0460b5dfe61e5f8a47476da
Brendan Isbell <brendan.isbell@automox.com>
2024-10-11 14:23:11 -0600

    Fixing "ValueError: Object 'EXCLUDE'" in https://github.com/NerdWalletOSS/dynamorm/issues/110. Also bumping Python to >=3.9

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.rst", "r") as readme_fd:
 
 setup(
     name="dynamorm",
-    version="0.11.0",
+    version="0.12.0",
     description="DynamORM is a Python object & relation mapping library for Amazon's DynamoDB service.",
     long_description=long_description,
     author="Evan Borgstrom",
     author_email="evan@borgstrom.ca",
     url="https://github.com/NerdWalletOSS/DynamORM",
     license="Apache License Version 2.0",
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4",
+    python_requires=">=2.7, >=3.9, <4",
     install_requires=["blinker>=1.4,<2.0", "boto3>=1.3,<2.0", "six"],
     extras_require={
         "marshmallow": ["marshmallow>=2.15.1,<4"],
@@ -22,10 +22,11 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
